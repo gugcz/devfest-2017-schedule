5fd66413619d7cf19777e412fd7f2d887692c0e0
Vit Listik <tivvit@seznam.cz>
2017-09-30 16:12:27 +0200

    finished speaker parser

--- parse_speakers.py
+++ parse_speakers.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
 import csv
+
+with open('speakers.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for r in reader:
+        print(r["Name"], r["Firma"], r["Název talku/workshopu"], r["Topic"])
